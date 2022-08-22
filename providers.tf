0824203825b9fc3f3e6db78d14846d7f385b2295
Jacob Nosal <jacobnosal@gmail.com>
2022-08-21 23:36:43 -0500

    Add support for state in s3.

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = ">= 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "tfstate-jnosal"
+    key    = "aws-vpc-terraform/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {}
